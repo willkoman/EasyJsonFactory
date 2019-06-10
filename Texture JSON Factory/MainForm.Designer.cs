414fb8ab13964f5868f5d913c2b85433387e2178
willkoman <willkoman@gmail.com>
2019-06-10 15:41:56 -0400

    Base Build

---
+++ Texture JSON Factory/MainForm.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Texture_JSON_Factory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_Block = new System.Windows.Forms.TabPage();
+            this.btn_South = new System.Windows.Forms.Button();
+            this.btn_East = new System.Windows.Forms.Button();
+            this.btn_West = new System.Windows.Forms.Button();
+            this.btn_Bottom = new System.Windows.Forms.Button();
+            this.btn_North = new System.Windows.Forms.Button();
+            this.btn_Top = new System.Windows.Forms.Button();
+            this.tab_Item = new System.Windows.Forms.TabPage();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tb_BlockName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_ModName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_Path = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tab_Block.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab_Block);
+            this.tabControl1.Controls.Add(this.tab_Item);
+            this.tabControl1.Location = new System.Drawing.Point(12, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(621, 308);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tab_Block
+            // 
+            this.tab_Block.Controls.Add(this.label1);
+            this.tab_Block.Controls.Add(this.tb_BlockName);
+            this.tab_Block.Controls.Add(this.btn_South);
+            this.tab_Block.Controls.Add(this.btn_East);
+            this.tab_Block.Controls.Add(this.btn_West);
+            this.tab_Block.Controls.Add(this.btn_Bottom);
+            this.tab_Block.Controls.Add(this.btn_North);
+            this.tab_Block.Controls.Add(this.btn_Top);
+            this.tab_Block.Location = new System.Drawing.Point(4, 22);
+            this.tab_Block.Name = "tab_Block";
+            this.tab_Block.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_Block.Size = new System.Drawing.Size(613, 282);
+            this.tab_Block.TabIndex = 0;
+            this.tab_Block.Text = "Block";
+            this.tab_Block.UseVisualStyleBackColor = true;
+            // 
+            // btn_South
+            // 
+            this.btn_South.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_South.Location = new System.Drawing.Point(298, 98);
+            this.btn_South.Name = "btn_South";
+            this.btn_South.Size = new System.Drawing.Size(88, 80);
+            this.btn_South.TabIndex = 11;
+            this.btn_South.Text = "South";
+            this.btn_South.UseVisualStyleBackColor = true;
+            this.btn_South.Click += new System.EventHandler(this.Btn_South_Click);
+            // 
+            // btn_East
+            // 
+            this.btn_East.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_East.Location = new System.Drawing.Point(204, 98);
+            this.btn_East.Name = "btn_East";
+            this.btn_East.Size = new System.Drawing.Size(88, 80);
+            this.btn_East.TabIndex = 10;
+            this.btn_East.Text = "East";
+            this.btn_East.UseVisualStyleBackColor = true;
+            this.btn_East.Click += new System.EventHandler(this.Btn_East_Click);
+            // 
+            // btn_West
+            // 
+            this.btn_West.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_West.Location = new System.Drawing.Point(16, 98);
+            this.btn_West.Name = "btn_West";
+            this.btn_West.Size = new System.Drawing.Size(88, 80);
+            this.btn_West.TabIndex = 9;
+            this.btn_West.Text = "West";
+            this.btn_West.UseVisualStyleBackColor = true;
+            this.btn_West.Click += new System.EventHandler(this.Btn_West_Click);
+            // 
+            // btn_Bottom
+            // 
+            this.btn_Bottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Bottom.Location = new System.Drawing.Point(110, 184);
+            this.btn_Bottom.Name = "btn_Bottom";
+            this.btn_Bottom.Size = new System.Drawing.Size(88, 80);
+            this.btn_Bottom.TabIndex = 8;
+            this.btn_Bottom.Text = "Bottom";
+            this.btn_Bottom.UseVisualStyleBackColor = true;
+            this.btn_Bottom.Click += new System.EventHandler(this.Btn_Bottom_Click);
+            // 
+            // btn_North
+            // 
+            this.btn_North.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_North.Location = new System.Drawing.Point(110, 98);
+            this.btn_North.Name = "btn_North";
+            this.btn_North.Size = new System.Drawing.Size(88, 80);
+            this.btn_North.TabIndex = 7;
+            this.btn_North.Text = "North";
+            this.btn_North.UseVisualStyleBackColor = true;
+            this.btn_North.Click += new System.EventHandler(this.Btn_North_Click);
+            // 
+            // btn_Top
+            // 
+            this.btn_Top.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Top.Location = new System.Drawing.Point(110, 12);
+            this.btn_Top.Name = "btn_Top";
+            this.btn_Top.Size = new System.Drawing.Size(88, 80);
+            this.btn_Top.TabIndex = 6;
+            this.btn_Top.Text = "Top";
+            this.btn_Top.UseVisualStyleBackColor = true;
+            this.btn_Top.Click += new System.EventHandler(this.Btn_Top_Click_1);
+            // 
+            // tab_Item
+            // 
+            this.tab_Item.Location = new System.Drawing.Point(4, 22);
+            this.tab_Item.Name = "tab_Item";
+            this.tab_Item.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_Item.Size = new System.Drawing.Size(613, 431);
+            this.tab_Item.TabIndex = 1;
+            this.tab_Item.Text = "Item";
+            this.tab_Item.UseVisualStyleBackColor = true;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Filter = "PNG File|*.png|All Files|*.*";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "PNG File|*.png|All Files|*.*";
+            // 
+            // tb_BlockName
+            // 
+            this.tb_BlockName.Location = new System.Drawing.Point(418, 129);
+            this.tb_BlockName.Name = "tb_BlockName";
+            this.tb_BlockName.Size = new System.Drawing.Size(168, 20);
+            this.tb_BlockName.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Block Name";
+            // 
+            // tb_ModName
+            // 
+            this.tb_ModName.Location = new System.Drawing.Point(16, 337);
+            this.tb_ModName.Name = "tb_ModName";
+            this.tb_ModName.Size = new System.Drawing.Size(165, 20);
+            this.tb_ModName.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Mod ID";
+            // 
+            // tb_Path
+            // 
+            this.tb_Path.Location = new System.Drawing.Point(220, 337);
+            this.tb_Path.Name = "tb_Path";
+            this.tb_Path.ReadOnly = true;
+            this.tb_Path.Size = new System.Drawing.Size(167, 20);
+            this.tb_Path.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Output Path";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 20);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btn_export
+            // 
+            this.btn_export.Location = new System.Drawing.Point(515, 336);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(114, 23);
+            this.btn_export.TabIndex = 18;
+            this.btn_export.Text = "Export!";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.Btn_export_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(640, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(293, 282);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 392);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_ModName);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tab_Block.ResumeLayout(false);
+            this.tab_Block.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab_Block;
+        private System.Windows.Forms.Button btn_South;
+        private System.Windows.Forms.Button btn_East;
+        private System.Windows.Forms.Button btn_West;
+        private System.Windows.Forms.Button btn_Bottom;
+        private System.Windows.Forms.Button btn_North;
+        private System.Windows.Forms.Button btn_Top;
+        private System.Windows.Forms.TabPage tab_Item;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tb_BlockName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_ModName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_Path;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Texture JSON Factory/MainForm.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Texture_JSON_Factory
+{
+    public partial class MainForm : Form
+    {
+
+        List<Bitmap> textures = new List<Bitmap>(6);
+        Bitmap[] itemTexture;
+        String path = @"";
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < textures.Capacity; i++)
+                textures.Add(new Bitmap(16, 16));
+        }
+
+
+        private void Btn_Top_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(0, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_Top.BackgroundImage = textures.ElementAt(0);
+        }
+
+        private void Btn_West_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(1, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_West.BackgroundImage = textures.ElementAt(1);
+        }
+
+        private void Btn_North_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(2, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_North.BackgroundImage = textures.ElementAt(2);
+        }
+
+        private void Btn_East_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(3, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_East.BackgroundImage = textures.ElementAt(3);
+        }
+
+        private void Btn_South_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(4, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_South.BackgroundImage = textures.ElementAt(4);
+        }
+
+        private void Btn_Bottom_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textures.Insert(5, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
+            btn_Bottom.BackgroundImage = textures.ElementAt(5);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            path = folderBrowserDialog1.SelectedPath;
+            tb_Path.Text = path;
+        }
+
+        private void Btn_export_Click(object sender, EventArgs e)
+        {
+            String permanentPath = path;
+            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\");
+            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\blockstates\\");
+            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\models\\block\\");
+            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\");
+            MessageBox.Show("Successfully Output to " + path + "\\" + tb_BlockName.Text + "\\" + "!");
+            createBlockModel(permanentPath);
+            createBlockState(permanentPath);
+
+            textures.ElementAt(0).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideUp.png", System.Drawing.Imaging.ImageFormat.Png);
+            textures.ElementAt(1).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideWest.png", System.Drawing.Imaging.ImageFormat.Png);
+            textures.ElementAt(2).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideNorth.png", System.Drawing.Imaging.ImageFormat.Png);
+            textures.ElementAt(3).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideEast.png", System.Drawing.Imaging.ImageFormat.Png);
+            textures.ElementAt(4).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideSouth.png", System.Drawing.Imaging.ImageFormat.Png);
+            textures.ElementAt(5).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideDown.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        public void createBlockModel(String path)
+        {
+            path = path + "\\" + tb_BlockName.Text + "\\models\\block\\";
+
+            Block block = new Block();
+            block.parent = "block/cube";
+            block.textures = new Textures();
+            block.textures.particle = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth";
+            block.textures.down = tb_ModName.Text.Trim(' ') + ":blocks/sideDown";
+            block.textures.up = tb_ModName.Text.Trim(' ') + ":blocks/sideUp";
+            block.textures.east = tb_ModName.Text.Trim(' ') + ":blocks/sideEast";
+            block.textures.west = tb_ModName.Text.Trim(' ') + ":blocks/sideWest";
+            block.textures.north = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth";
+            block.textures.south = tb_ModName.Text.Trim(' ') + ":blocks/sideSouth";
+
+            String converted = JsonConvert.SerializeObject(block);
+            //richTextBox1.Text = JToken.Parse(converted).ToString();
+            File.WriteAllText(path + tb_BlockName.Text + ".json", JToken.Parse(converted).ToString());
+        }
+
+        public void createBlockState(String path)
+        {
+            path = path + "\\" + tb_BlockName.Text + "\\blockstates\\";
+            BlockState bs = new BlockState();
+            bs.defaults = new Defaults();
+            bs.variants = new Variants();
+            bs.variants.facing = new Facing();
+            bs.variants.facing.south = new South();
+            bs.variants.facing.north = new North();
+            bs.variants.facing.east = new East();
+            bs.variants.facing.west = new West();
+            bs.variants.facing.up = new Up();
+            bs.variants.facing.down = new Down();
+
+            bs.forge_marker = 1;
+            bs.defaults.model = tb_ModName.Text + ":" + tb_BlockName.Text;
+            bs.variants.normal = null;
+            bs.variants.inventory = null;
+            bs.variants.facing.north = null;
+            bs.variants.facing.south.y = 180;
+            bs.variants.facing.west.y = 270;
+            bs.variants.facing.east.y = 90;
+            bs.variants.facing.up.x = -90;
+            bs.variants.facing.down.x = 90;
+
+            String converted = JsonConvert.SerializeObject(bs);
+            //richTextBox1.Text = JToken.Parse(converted).ToString();
+            File.WriteAllText(path + tb_BlockName.Text + ".json", JToken.Parse(converted).ToString());
+        }
+
+        internal class Block
+        {
+            public string parent { get; set; }
+            public Textures textures { get; set; }
+
+
+        }
+        public class Textures
+        {
+            public string particle { get; set; }
+            public string down { get; set; }
+            public string up { get; set; }
+            public string east { get; set; }
+            public string west { get; set; }
+            public string north { get; set; }
+            public string south { get; set; }
+        }
+        public class Defaults
+        {
+            public string model { get; set; }
+        }
+
+        public class Normal
+        {
+        }
+
+        public class Inventory
+        {
+        }
+
+        public class North
+        {
+        }
+
+        public class South
+        {
+            public int y { get; set; }
+        }
+
+        public class West
+        {
+            public int y { get; set; }
+        }
+
+        public class East
+        {
+            public int y { get; set; }
+        }
+
+        public class Up
+        {
+            public int x { get; set; }
+        }
+
+        public class Down
+        {
+            public int x { get; set; }
+        }
+
+        public class Facing
+        {
+            public North north { get; set; }
+            public South south { get; set; }
+            public West west { get; set; }
+            public East east { get; set; }
+            public Up up { get; set; }
+            public Down down { get; set; }
+        }
+
+        public class Variants
+        {
+            public IList<Normal> normal { get; set; }
+            public IList<Inventory> inventory { get; set; }
+            public Facing facing { get; set; }
+        }
+
+        public class BlockState
+        {
+            public int forge_marker { get; set; }
+            public Defaults defaults { get; set; }
+            public Variants variants { get; set; }
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (path == "" || tb_BlockName.Text.Trim(' ') == "" || tb_ModName.Text.Trim(' ') == "")
+                btn_export.Enabled = false;
+            else
+                btn_export.Enabled = true;
+
+
+        }
+    }
+}
