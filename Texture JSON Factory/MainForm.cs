0207da9014901e02a551749be4092e24b94b12ff
willkoman <willkoman@gmail.com>
2019-06-11 14:24:54 -0400

    Block Creation Finalized
    TODO: Finish item creation

--- Texture JSON Factory/MainForm.cs
+++ Texture JSON Factory/MainForm.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Security.AccessControl;
+using System.Drawing.Imaging;
 
 namespace Texture_JSON_Factory
 {
@@ -23,7 +25,7 @@ namespace Texture_JSON_Factory
         {
             InitializeComponent();
             for (int i = 0; i < textures.Capacity; i++)
-                textures.Add(new Bitmap(16, 16));
+                textures.Insert(i,new Bitmap(16, 16));
         }
 
 
@@ -32,6 +34,7 @@ namespace Texture_JSON_Factory
             openFileDialog1.ShowDialog();
             textures.Insert(0, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
             btn_Top.BackgroundImage = textures.ElementAt(0);
+            
         }
 
         private void Btn_West_Click(object sender, EventArgs e)
@@ -67,6 +70,7 @@ namespace Texture_JSON_Factory
             openFileDialog1.ShowDialog();
             textures.Insert(5, (Bitmap)Bitmap.FromFile(openFileDialog1.FileName));
             btn_Bottom.BackgroundImage = textures.ElementAt(5);
+            
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -79,57 +83,111 @@ namespace Texture_JSON_Factory
         private void Btn_export_Click(object sender, EventArgs e)
         {
             String permanentPath = path;
-            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\");
-            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\blockstates\\");
-            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\models\\block\\");
-            Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\");
+            var baseDir = Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\");
+            baseDir.Attributes = FileAttributes.Normal;
+            var modelDir = Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\models\\block\\");
+            modelDir.Attributes = FileAttributes.Normal;
+            var blockstateDir = Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\blockstates\\");
+            blockstateDir.Attributes = FileAttributes.Normal;
+            var textureDir = Directory.CreateDirectory(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\"+tb_BlockName.Text);
+            textureDir.Attributes &= ~FileAttributes.ReadOnly;
+
+            CreateBlockModel(permanentPath);
+            CreateBlockState(permanentPath);
+
+            SavePNG(textureDir.FullName + "\\" + "sideUp.png", textures.ElementAt(0), 100);
+            SavePNG(textureDir.FullName + "\\" + "sideWest.png", textures.ElementAt(1), 100);
+            SavePNG(textureDir.FullName + "\\" + "sideNorth.png", textures.ElementAt(2), 100);
+            SavePNG(textureDir.FullName + "\\" + "sideEast.png", textures.ElementAt(3), 100);
+            SavePNG(textureDir.FullName + "\\" + "sideSouth.png", textures.ElementAt(4), 100);
+            SavePNG(textureDir.FullName + "\\" + "sideDown.png", textures.ElementAt(5), 100);
+            //textures.ElementAt(0).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideUp.png", System.Drawing.Imaging.ImageFormat.Jpeg);
+            //textures.ElementAt(1).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideWest.png", System.Drawing.Imaging.ImageFormat.Png);
+            //textures.ElementAt(2).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideNorth.png", System.Drawing.Imaging.ImageFormat.Png);
+            //textures.ElementAt(3).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideEast.png", System.Drawing.Imaging.ImageFormat.Png);
+            //textures.ElementAt(4).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideSouth.png", System.Drawing.Imaging.ImageFormat.Png);
+            //textures.ElementAt(5).Save(textureDir.FullName + tb_BlockName.Text + "\\" + "sideDown.png", System.Drawing.Imaging.ImageFormat.Png);
             MessageBox.Show("Successfully Output to " + path + "\\" + tb_BlockName.Text + "\\" + "!");
-            createBlockModel(permanentPath);
-            createBlockState(permanentPath);
 
-            textures.ElementAt(0).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideUp.png", System.Drawing.Imaging.ImageFormat.Png);
-            textures.ElementAt(1).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideWest.png", System.Drawing.Imaging.ImageFormat.Png);
-            textures.ElementAt(2).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideNorth.png", System.Drawing.Imaging.ImageFormat.Png);
-            textures.ElementAt(3).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideEast.png", System.Drawing.Imaging.ImageFormat.Png);
-            textures.ElementAt(4).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideSouth.png", System.Drawing.Imaging.ImageFormat.Png);
-            textures.ElementAt(5).Save(path + "\\" + tb_BlockName.Text + "\\textures\\blocks\\" + tb_BlockName.Text + "\\" + "sideDown.png", System.Drawing.Imaging.ImageFormat.Png);
         }
+        public static void SavePNG (string path, Image img, int quality)
+        {
+            EncoderParameter qualityParam
+            = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
 
-        public void createBlockModel(String path)
+            ImageCodecInfo jpegCodec= GetEncoderInfo(@"image/png");
+
+            EncoderParameters encoderParams
+            = new EncoderParameters(1);
+
+            encoderParams.Param[0] = qualityParam;
+
+            System.IO.MemoryStream mss = new System.IO.MemoryStream();
+
+            System.IO.FileStream fs
+            = new System.IO.FileStream(path, System.IO.FileMode.Create
+            , System.IO.FileAccess.ReadWrite);
+
+            img.Save(mss, jpegCodec, encoderParams);
+            byte[] matriz = mss.ToArray();
+            fs.Write(matriz, 0, matriz.Length);
+
+            mss.Close();
+            fs.Close();
+        }
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+        public void CreateBlockModel(String path)
         {
             path = path + "\\" + tb_BlockName.Text + "\\models\\block\\";
 
-            Block block = new Block();
-            block.parent = "block/cube";
-            block.textures = new Textures();
-            block.textures.particle = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth";
-            block.textures.down = tb_ModName.Text.Trim(' ') + ":blocks/sideDown";
-            block.textures.up = tb_ModName.Text.Trim(' ') + ":blocks/sideUp";
-            block.textures.east = tb_ModName.Text.Trim(' ') + ":blocks/sideEast";
-            block.textures.west = tb_ModName.Text.Trim(' ') + ":blocks/sideWest";
-            block.textures.north = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth";
-            block.textures.south = tb_ModName.Text.Trim(' ') + ":blocks/sideSouth";
+            Block block = new Block
+            {
+                parent = "block/cube",
+                textures = new Textures
+                {
+                    particle = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth",
+                    down = tb_ModName.Text.Trim(' ') + ":blocks/sideDown",
+                    up = tb_ModName.Text.Trim(' ') + ":blocks/sideUp",
+                    east = tb_ModName.Text.Trim(' ') + ":blocks/sideEast",
+                    west = tb_ModName.Text.Trim(' ') + ":blocks/sideWest",
+                    north = tb_ModName.Text.Trim(' ') + ":blocks/sideNorth",
+                    south = tb_ModName.Text.Trim(' ') + ":blocks/sideSouth"
+                }
+            };
 
             String converted = JsonConvert.SerializeObject(block);
             //richTextBox1.Text = JToken.Parse(converted).ToString();
             File.WriteAllText(path + tb_BlockName.Text + ".json", JToken.Parse(converted).ToString());
         }
 
-        public void createBlockState(String path)
+        public void CreateBlockState(String path)
         {
             path = path + "\\" + tb_BlockName.Text + "\\blockstates\\";
-            BlockState bs = new BlockState();
-            bs.defaults = new Defaults();
-            bs.variants = new Variants();
-            bs.variants.facing = new Facing();
-            bs.variants.facing.south = new South();
-            bs.variants.facing.north = new North();
-            bs.variants.facing.east = new East();
-            bs.variants.facing.west = new West();
-            bs.variants.facing.up = new Up();
-            bs.variants.facing.down = new Down();
-
-            bs.forge_marker = 1;
+            BlockState bs = new BlockState
+            {
+                defaults = new Defaults(),
+                variants = new Variants()
+            };
+            bs.variants.facing = new Facing
+            {
+                south = new South(),
+                north = new North(),
+                east = new East(),
+                west = new West(),
+                up = new Up(),
+                down = new Down()
+            };
             bs.defaults.model = tb_ModName.Text + ":" + tb_BlockName.Text;
             bs.variants.normal = null;
             bs.variants.inventory = null;
